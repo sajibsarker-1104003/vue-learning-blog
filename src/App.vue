6aead38e0117aed2aec52f5f1dc7a4e033f6e9ea
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-03-11 18:22:58 +0600

    Blog User Interface

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div id="app">
+<Header/>
+<Latest/>
+<Posts/>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header';
+import Latest from './components/Latest.vue';
+import Posts from './components/Posts.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,
+    Latest,
+    Posts,
+  
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss" scoped>
+body{
+  background-color:#F3F3F3;
+  font-family:"Rockwell" serif;
+
+  h3{
+    color:#171717;
+    font-size:28px;
+    font-weight:700;
+  }
 }
 </style>
